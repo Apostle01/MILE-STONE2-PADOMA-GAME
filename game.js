687d47519b78cdc65ee7f78f9a1688d65b6c7a14
Apostle01 <148454484+Apostle01@users.noreply.github.com>
2024-05-10 00:25:39 +0100

    Add files via upload

---
+++ game.js
@@ -0,0 +1,9 @@
+const game = {
+  score: 0,
+  currentGame: {},
+  playerMoves: [],
+  choices: ["button1", "button2", "button3", "button4"]
+};
+const { game } = require('./game.js');
+
+module.exports = { game };
